81db6571a8d6546f5790c486c2d082b2cf36b7f6
ductt <ductt@intern.firegroup.io>
2025-01-07 17:54:03 +0700

    deploy

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+
 import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-    // i18next-http-backend
-    // loads translations from your server
-    // https://github.com/i18next/i18next-http-backend
+    // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+    // learn more: https://github.com/i18next/i18next-http-backend
+    // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
     .use(Backend)
     // detect user language
     // learn more: https://github.com/i18next/i18next-browser-languageDetector
@@ -16,9 +17,13 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
-        debug: false,
         fallbackLng: "en",
-        lng: "en", // default language
+        debug: false,
+        backend: {
+            loadPath: "/my-portfolio/locales/{{lng}}/{{ns}}.json",
+        },
+        ns: ["translation"],
+        defaultNS: "translation",
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
         },
